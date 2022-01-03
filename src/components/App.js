21f4962efd172967719d15f70128491a86b65a83
subindevk <subindevk@debugsbunny.com>
2022-01-03 20:25:00 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
- import { BrowserRouter as Router , Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { uuid } from "uuidv4";
 import "./App.css";
 import Header from "./Header";
@@ -7,67 +7,56 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
+	const LOCAL_STORAGE_KEY = "contacts";
+	const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, { id: uuid(), ...contact }]);
-  };
+	const addContactHandler = (contact) => {
+		setContacts([...contacts, { id: uuid(), ...contact }]);
+	};
 
-  const removeContactHandler = (id) => {
-    const newContactList = contacts.filter((contact) => {
-      return contact.id !== id;
-    });
+	const removeContactHandler = (id) => {
+		const newContactList = contacts.filter((contact) => {
+			return contact.id !== id;
+		});
 
-    setContacts(newContactList);
-  };
+		setContacts(newContactList);
+	};
 
-  useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retriveContacts) setContacts(retriveContacts);
-  }, []);
+	useEffect(() => {
+		const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+		if (retriveContacts) setContacts(retriveContacts);
+	}, []);
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+	}, [contacts]);
 
-  return (
-    <div className="ui container">
-      <Router>
-      <Header />
-      <Switch>
-      <Route
-      exact
-      path='/'
-      render={(props)=>{
-        <ContactList
-        {...props}
-        contacts={contacts}
-        getContactId={removeContactHandler}
-        />
-      }}
-      >
-     
-     {/* This is routing */}
-      </Route>
-      <Route
-      path='/add'
-      exact
-      render={(props)=>{
-        <AddContact
-        {...props}
-        addContactHandler={addContactHandler}
-        />
-      }}
-      >  
-      </Route>
-      </Switch>
-      {/* <AddContact addContactHandler={addContactHandler} /> */}
-      {/* <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
-      </Router>
-    </div>
-  );
+	return (
+		<div className="ui container">
+			<Router>
+				<Header />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<ContactList
+								contacts={contacts}
+								getContactId={removeContactHandler}
+							/>
+						}
+					></Route>
+					<Route
+						path="/add"
+						exact
+						element={<AddContact addContactHandler={addContactHandler} />}
+					></Route>
+				</Routes>
+				{/* <AddContact addContactHandler={addContactHandler} /> */}
+				{/* <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+			</Router>
+		</div>
+	);
 }
 
 export default App;
