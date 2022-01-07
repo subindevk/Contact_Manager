68577781a7827cbdd352d6424439dde13aaaa53f
subindevk <subindevk@debugsbunny.com>
2022-01-07 13:22:38 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,7 @@ function App() {
 						element={<AddContact addContactHandler={addContactHandler} />}
 					></Route> 
 					<Route
-					   path='/contact/:id' exact element={<ContactDetails />}>
+					   path='/contact/:name' exact element={<ContactDetails />}>
 
 					   </Route>
 				</Routes>
--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useParams } from "react-router-dom";
 import user from "../images/user.png";
 
-const ContactDetails = (props) => {
-    //  const{name,email} = props.contact
-  return (
-   <div className="main">
-       <div className="ui main centered">
-           <div className="image">
-               <img src={user} alt="user">
-
-               </img>
-           </div>
-           <div className="content">
-               <div className="header">
-                   asaf
-               </div>
-               <div className="description">
-                   asfaf
-               </div>
-           </div>
-       </div>
-       <div className="center-div">
-           <Link to='/'>
-           <button className="ui button blue center">Back to Contact</button>
-           </Link>
-       </div>
-   </div>
-  );
-};
+function ContactDetails(){
+    let {name,email} = useParams();
+    return (
+        <div className="main">
+            <div className="ui main centered">
+                <div className="image">
+                    <img src={user} alt="user">
+                        
+                    </img>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {name}
+                    </div>
+                    <div className="description">
+                        {email}
+                    </div>
+                </div>
+            </div>
+            <div className="center-div">
+                <Link to='/'>
+                <button className="ui button blue center">Back to Contact</button>
+                </Link>
+            </div>
+        </div>
+       );
+}
 
 export default ContactDetails;
